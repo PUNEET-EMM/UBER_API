44eb2d8e9708ecdb93f9173da64b09d3340b3321
Puneet <puneet19338@gmail.com>
2024-07-23 11:09:32 +0530

    Code Structure

---
+++ src/test/java/com/example/project/uber/uberApi/UberApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project.uber.uberApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
